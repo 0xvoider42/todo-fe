61976d978bd6e6c95c6731ddee1492ca6b689e78
0xVoider42 <shautidzenikoloz@gmail.com>
2022-11-09 17:30:27 +0200

    adds ui elements

--- src/components/UI/TodoTable.tsx
+++ src/components/UI/TodoTable.tsx
@@ -6,19 +6,38 @@ import {
   TableCell,
   TableBody,
   Paper,
+  Alert,
+  Snackbar,
 } from "@mui/material";
 import { useMutation } from "react-query";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import { deleteTodo } from "../../services/queries/delete-todo";
 import { receiveTodo } from "../../models/todo";
+import { useState } from "react";
 
 const TodoTable: ({ todos }: { todos: receiveTodo[] }) => JSX.Element = ({
   todos,
 }) => {
+  const [alert, setAlert] = useState(false);
+
   const { mutate } = useMutation(deleteTodo);
 
   return (
     <Paper elevation={2}>
+      {alert ? (
+        <Snackbar open={alert} autoHideDuration={3000}>
+          <Alert
+            severity="error"
+            onClose={() => {
+              setAlert(false);
+            }}
+          >
+            Todo has been deleted
+          </Alert>
+        </Snackbar>
+      ) : null}
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -28,6 +47,7 @@ const TodoTable: ({ todos }: { todos: receiveTodo[] }) => JSX.Element = ({
               <TableCell align="right">Text</TableCell>
               <TableCell align="right">Created</TableCell>
               <TableCell align="right">Updated</TableCell>
+              <TableCell align="right">Delete</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -35,7 +55,6 @@ const TodoTable: ({ todos }: { todos: receiveTodo[] }) => JSX.Element = ({
               <TableRow
                 key={todo.id}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                onClick={() => mutate({ id: todo.id })}
               >
                 <TableCell component="th" scope="row">
                   {todo.id}
@@ -44,6 +63,19 @@ const TodoTable: ({ todos }: { todos: receiveTodo[] }) => JSX.Element = ({
                 <TableCell align="right">{todo.text}</TableCell>
                 <TableCell align="right">{todo.createdAt}</TableCell>
                 <TableCell align="right">{todo.updatedAt}</TableCell>
+                <TableCell align="right">
+                  <IconButton
+                    aria-label="delete"
+                    size="medium"
+                    color="error"
+                    onClick={() => {
+                      mutate({ id: todo.id });
+                      setAlert(true);
+                    }}
+                  >
+                    <DeleteIcon fontSize="small" />
+                  </IconButton>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
