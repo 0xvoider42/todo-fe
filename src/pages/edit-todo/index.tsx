b31df1ee5ef3118e00279a255ed2028b950e66c7
0xVoider42 <shautidzenikoloz@gmail.com>
2022-11-16 14:57:49 +0200

    updates folder structure

--- src/pages/edit-todo/index.tsx
+++ src/pages/edit-todo/index.tsx
@@ -1,8 +1,80 @@
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useSnackbar } from "material-ui-snackbar-provider";
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
 import withAuthRedirect from "../../hoc/auth-redirect";
-import InputField from "../../components/edit-todo";
 
-const EditTodoPage = () => {
-  return <InputField />;
+import { ApiTodo } from "../../models/todo";
+import { editTodo } from "../../services/queries/edit-todo";
+
+const UpdateTodo = () => {
+  const { mutate } = useMutation(editTodo);
+
+  const snackbar = useSnackbar();
+
+  const { register, handleSubmit } = useForm({});
+
+  const submitHandler = (data: ApiTodo) => {
+    mutate({ ...data });
+    snackbar.showMessage("Todo has been edited!");
+  };
+
+  return (
+    <Paper elevation={2}>
+      <Container maxWidth="md">
+        <Box padding={2}>
+          <form onSubmit={handleSubmit(submitHandler)}>
+            <Stack spacing={1.5} alignItems="baseline">
+              <Grid container direction="column" item xs={2}>
+                <TextField
+                  {...register("id", { required: true })}
+                  required
+                  id="outlined-multiline-flexible"
+                  label="ID"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid container item xs={6}>
+                <TextField
+                  {...register("title")}
+                  id="outlined-basic"
+                  fullWidth
+                  label="Title"
+                  variant="outlined"
+                  type="text"
+                />
+              </Grid>
+              <Grid container item xs={6}>
+                <TextField
+                  {...register("text")}
+                  multiline
+                  rows={3}
+                  fullWidth
+                  id="outlined-multiline-static"
+                  label="Text"
+                  variant="outlined"
+                  type="text"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <Button type="submit" variant="outlined">
+                  Submit
+                </Button>
+              </Grid>
+            </Stack>
+          </form>
+        </Box>
+      </Container>
+    </Paper>
+  );
 };
 
-export default withAuthRedirect(EditTodoPage);
+export default withAuthRedirect(UpdateTodo);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Tooltip,
+} from "@mui/material";
+import { useMutation } from "react-query";
+import { useSnackbar } from "material-ui-snackbar-provider";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+
+import { deleteTodo } from "../services/queries/delete-todo";
 import { ApiTodo, ReceiveTodo } from "../models/todo";
+import useUserInfo from "../hooks/user-info";
+import { useState } from "react";
 import { getTodos } from "../services/queries/get-todos";
 import withAuthRedirect from "../hoc/auth-redirect";
-import TodoTable from "../components/todo-table";
-
-interface Props {
-  todos: ReceiveTodo[];
-}
 
 export const getServerSideProps = async (ctx) => {
   const data: ApiTodo[] = await getTodos();
@@ -17,8 +30,73 @@ export const getServerSideProps = async (ctx) => {
   };
 };
 
-const HomePage = ({ todos }: Props) => {
-  return <TodoTable todos={todos} />;
+const TodoTable: ({ todos }: { todos: ReceiveTodo[] }) => JSX.Element = ({
+  todos,
+}) => {
+  const [Todos, setTodos] = useState(todos);
+  const { isLoggedIn } = useUserInfo();
+
+  const snackbar = useSnackbar();
+
+  const { mutate } = useMutation(deleteTodo);
+
+  const handleDelete = (id) => {
+    Todos.splice(id, 1);
+    setTodos(Todos);
+  };
+
+  return (
+    <Paper elevation={2} sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ minWidth: 650, maxHeight: 600 }}>
+        <Table stickyHeader aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Todo ID</TableCell>
+              <TableCell align="right">Title</TableCell>
+              <TableCell align="right">Text</TableCell>
+              <TableCell align="right">Created</TableCell>
+              <TableCell align="right">Updated</TableCell>
+              {isLoggedIn && <TableCell align="right">Delete</TableCell>}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Todos.map((todo: ReceiveTodo) => (
+              <TableRow
+                key={todo.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {todo.id}
+                </TableCell>
+                <TableCell align="right">{todo.title}</TableCell>
+                <TableCell align="right">{todo.text}</TableCell>
+                <TableCell align="right">{todo.createdAt}</TableCell>
+                <TableCell align="right">{todo.updatedAt}</TableCell>
+                {isLoggedIn && (
+                  <TableCell align="right">
+                    <Tooltip title="Delete">
+                      <IconButton
+                        aria-label="delete"
+                        size="medium"
+                        color="error"
+                        onClick={() => {
+                          mutate({ id: todo.id });
+                          handleDelete({ id: todo.id });
+                          snackbar.showMessage("Todo has been deleted!");
+                        }}
+                      >
+                        <DeleteIcon fontSize="small" />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 };
 
-export default withAuthRedirect(HomePage);
+export default withAuthRedirect(TodoTable);
